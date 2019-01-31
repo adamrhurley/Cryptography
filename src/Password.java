7b9fc183df83b2613cb9abc633c57b3f6238a28f
t00198396 <adam.hurley@students.ittralee.ie>
2019-01-31 17:48:28 +0000

    V2

--- src/Password.java
+++ src/Password.java
@@ -1,5 +1,33 @@
 public class Password{
-    public static void main(String[] args) {
+   public static String encrypt(String plaintext, int numColumns)
+   {
+       int numX;
+       int numRows;
+       String strNoSpace="";
+        for(int i=0; i < plaintext.length(); i++)
+        {
+            if(plaintext.charAt(i) != ' ')
+                strNoSpace  += plaintext.charAt(i);
+        }
 
-    }
+
+        if (strNoSpace.length() % numColumns == 0)
+        numRows = strNoSpace.length()/numColumns;
+
+        else
+            numRows = (strNoSpace.length()/numColumns) + 1;
+
+
+        numX = numColumns - (strNoSpace.length() % numColumns);
+
+        for(int i=0; i < numX; i++)
+        {
+            strNoSpace += "X";
+        }
+
+       System.out.println(numRows);
+
+
+        return strNoSpace;
+   }
 }
---
+++ src/encrypt.java
@@ -0,0 +1,7 @@
+public class encrypt {
+    public static void main(String[] args) {
+        String result = Password.encrypt("THIS IS A SECRET MESSAGE", 4);
+        System.out.println(result);
+    }
+
+}
